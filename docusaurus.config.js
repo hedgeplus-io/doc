bca51a1850c67eb6da5142f5adf8043932c202dc
PaulieB14 <94752445+PaulieB14@users.noreply.github.com>
2022-09-17 11:42:05 -0400

    fixed sidebar position

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -17,7 +17,7 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'paulieb14', // Usually your GitHub org/user name.
-  projectName: 'hedgeplus-io-docs', // Usually your repo name.
+  projectName: 'Hedgeplus-docs', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
